9f7a68c002b48199bcb957ca8825506ce797b624
David Li <david.li@stud.uni-heidelberg.de>
2022-11-27 21:50:33 +0100

    Small changes

--- euclideanGeometry.py
+++ euclideanGeometry.py
@@ -37,5 +37,5 @@ c = EuclideanGeometry(0)
 
 p1 = [1, 6]
 p2 = [3,4]
-print(EuclideanGeometry.translate(c, p1, p2))
-print(EuclideanGeometry.direction(c, p1, p2))
+print(c.translate( p1, p2))
+print(c.direction(p1, p2))
