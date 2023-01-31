41689acae283282cd2789599ddf38a4a6629ea42
annaroth01 <116730215+annaroth01@users.noreply.github.com>
2023-01-31 11:09:06 +0100

    Update run.py

--- run.py
+++ run.py
@@ -8,18 +8,23 @@ import numpy as np
 import edgelist as el
 import graphlayout as gl
 import forceatlas2 as fa
+import draw as d
 
 # Takes the filename of the graph, (runs it through the algorithm and returns corresponding graphlayout) -todo
 def run(filename):
     graph = el.edglist(filename)
-    coords = fa.forceatlas2(graph,20,10,0.1,10,0.1)
-    gl.layout(filename+'result', graph, coords)
-
+    coords = []
+    # Arguments: graph, setup, repulsion const., gravity const., swing const., max. swing, swing tolerance
+    for i in range(0,500):
+        coords = fa.forceatlas2(graph,coords,2,30,0.1,10,0.5)
+        #gl.layout(filename+str(i)+'result', graph, coords)
+        c = d.DrawPoincareDisk(500, filename+"result/"+str(i)+".png")
+        c.plotGraph(graph, coords)
 
 # Testing cases:
-#tests = ["testgraphs/petersen_graph","testgraphs/tree_like","testgraphs/self_loop","testgraphs/disconnected","testgraphs/square_tail","testgraphs/square_complete","testgraphs/square_empty","testgraphs/single_edge","testgraphs/generic_graph"]
+#tests = ["testgraphs/petersen_graph","testgraphs/tree_like","testgraphs/self_loop","testgraphs/disconnected","testgraphs/square_tail","testgraphs/square_complete","testgraphs/single_edge","testgraphs/generic_graph"]
 #for x in tests:
 #    g = run(x)
 #    graph_draw(g, vertex_text=g.vp.name, output="results/"+x+".pdf")
-run("testgraphs/single_edge")
+run("testgraphs/petersen_graph")
 # Cases that need handling: ,"testgraphs/empty_graph"
