464094aa231b1516230070b336e87d9e8d975942
annaroth01 <116730215+annaroth01@users.noreply.github.com>
2023-02-13 13:45:02 +0100

    Update run.py
    - Added code for getting a GIF
    - Comment added regarding constants

--- run.py
+++ run.py
@@ -10,27 +10,47 @@ import graphlayout as gl
 import fa2general as fa
 import euclideanGeometry as eG
 import PoincareDiskModel as pdm
+#from PIL import Image  #Uncomment for GIF - needs package
 import gi.repository 
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
+
 # Takes the filename of the graph, (runs it through the algorithm and returns corresponding graphlayout) -todo
-def run(filename):
+def run(filename, it):
     graph = el.edglist(filename)
     coords = []
     # Arguments: graph, setup, repulsion const., gravity const., swing const., max. swing, swing tolerance
-    for i in range(0,500):
+        # Repulsion Constant: Integers for eG, <<<1 for pdm
+        # The more vertices in the graph, The higher the total energy in the graph; one has to compensate this by decreasing the repulsion for higher vertex counts
+        # Be aware that the graphs react *very* sensitive to changes in the constants
+        
+    for i in range(0,it):
         coords = fa.forceatlas2(graph, coords, 2,30,0.1,10,0.5)
         #gl.layout(filename+str(i)+'result', graph, coords)
         c = pdm.Interface(graph, coords, 1000)
+        #PDM = pdm.PoincareDiskModel([0,0]) # Uncomment for GIF
+        #PDM.drawGraph(graph, coords, "filename+"result/"+str(i)+".png", 1000) #Uncomment for GIF
         Gtk.main()
         coords = c.points
-        #PDM = pdm.PoincareDiskModel([0,0])
-        #PDM.drawGraph(graph, coords, "filename+"result/"+str(i)+".png", 10000)
+    #toGif(filename, it) # Takes A LOT of time - uncomment for GIF
+
+# Uncomment for GIF   
+#def toGif(filename, it):
+#    imagelist = []
+
+#    for i in range(0,it):
+#        img = Image.open(filename+"result/"+str(i)+".png", mode='r', formats=None)
+#        img.save(filename+"result/"+str(i)+".jpg")
+#        imagelist += [filename+"result/"+str(i)+".jpg"]
+#    clip = ImageSequenceClip(imagelist, fps=5)
+#    clip.write_gif(filename+'.gif', fps=5)
+        
+        
 
 # Testing cases:
 #tests = ["testgraphs/petersen_graph","testgraphs/tree_like","testgraphs/self_loop","testgraphs/disconnected","testgraphs/square_tail","testgraphs/square_complete","testgraphs/single_edge","testgraphs/generic_graph"]
 #for x in tests:
 #    g = run(x)
 #    graph_draw(g, vertex_text=g.vp.name, output="results/"+x+".pdf")
-run("testgraphs/petersen_graph")
+run("testgraphs/petersen_graph", 50)
 # Cases that need handling: ,"testgraphs/empty_graph"
